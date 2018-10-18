1399e334ccf4392a3c878c31efbf43d440963107
thomkok20 <tkok@roc-dev.com>
2018-10-18 10:03:58 +0200

    update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,14 +1,31 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
         <a id="projectNaam" class="navbar-brand" href="{{ url('/') }}">Radiostation</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                @auth
+                    @if (Auth::user()->permission_id == 0)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin') }}">{{ __('Admin') }}</a>
+                        </li>
+                    @elseif (Auth::user()->permission_id == 1)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('programma') }}">{{ __('Programma') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('liedje') }}">{{ __('Liedje') }}</a>
+                        </li>
+                    @endif
+                @endauth
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -28,12 +45,6 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            @if (Auth::user()->permission_id == 0)
-                                <a id="home" class="dropdown-item" href="{{ route('admin') }}">{{ __('Admin') }}</a>
-                            @elseif (Auth::user()->permission_id == 1)
-                                <a id="home" class="dropdown-item" href="{{ route('home') }}">{{ __('Dashboard') }}</a>
-                            @endif
-
                             <a id="logout" class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                         </div>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
+Route::get('/programma', 'ProgrammaController@index')->name('programma');
+Route::get('/liedjes', 'LiedjesController@index')->name('liedje');
