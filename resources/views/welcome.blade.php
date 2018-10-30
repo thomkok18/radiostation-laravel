955685a96873dcb81aa691406e2d213cfbab5163
thomkok20 <tkok@roc-dev.com>
2018-10-30 17:00:52 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 id="programmaNaam" class="modal-title">Weet u zeker?</h5>
+                    <h5 id="programmaNaam" class="modal-title">Weet u het zeker?</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
